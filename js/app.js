566e8193afd9d955be4d0c7f7f36bfe5b795cad4
Nanda Kishore <kishoreofficialone@gmail.com>
2018-09-13 05:03:21 +0530

    New Interface

--- js/app.js
+++ js/app.js
@@ -1,51 +1,101 @@
-var app = angular.module('starter', ['ui.router','starter.controllers'])
+// Ionic Starter App
 
-app.config(function($stateProvider, $urlRouterProvider) {
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
+.config(function ($ionicConfigProvider) {
+  $ionicConfigProvider.tabs.position('bottom')
+  $ionicConfigProvider.tabs.style('standard')
+})
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
   $stateProvider
 
-    .state('main', {
-        url: '',
-        templateUrl: 'templates/about.html',
-        controller : "AboutCtrl"
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.skills', {
+    url: '/skills',
+    views: {
+      'tab-skills': {
+        templateUrl: 'templates/tab-skills.html',
+        controller: 'SkillsCtrl'
+      }
+    }
+  })
+
+  .state('tab.acheivements', {
+      url: '/acheivements',
+      views: {
+        'tab-acheivements': {
+          templateUrl: 'templates/tab-acheivements.html',
+          controller: 'AcheivementsCtrl'
+        }
+      }
     })
 
-    .state('about', {
-        url: '/about',
-        templateUrl: 'templates/about.html',
-        controller : "AboutCtrl"
-    })
+  .state('tab.about-me', {
+    url: '/about-me',
+    views: {
+      'tab-about-me': {
+        templateUrl: 'templates/tab-about-me.html',
+        controller: 'AboutMeCtrl'
+      }
+    }
+  })
 
-    .state('projects', {
-        url: '/projects/:ProjectId',
-        templateUrl: 'templates/projects.html',
-        controller : "ProjectsCtrl"
-    })
+  .state('tab.experience', {
+    url: '/experience',
+    views: {
+      'tab-experience': {
+        templateUrl: 'templates/tab-experience.html',
+        controller: 'ExperienceCtrl'
+      }
+    }
+  })
 
-    .state('experience', {
-        url: '/experience',
-        templateUrl: 'templates/experience.html',
-        controller : "ExperienceCtrl"
-    })
+  .state('tab.projects', {
+    url: '/projects',
+    views: {
+      'tab-projects': {
+        templateUrl: 'templates/tab-projects.html',
+        controller: 'ProjectsCtrl'
+      }
+    }
+  })
 
-    .state('skills', {
-        url: '/skills',
-        templateUrl: 'templates/skills.html',
-        controller : "SkillsCtrl"
-    })
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tab/about-me')
 
-    .state('achievements', {
-        url: '/achievements',
-        templateUrl: 'templates/achievements.html',
-        controller : "AchievementsCtrl"
-    })
-
-    .state('resume', {
-        url: '/resume',
-        templateUrl: 'templates/resume.html',
-        controller : "ResumeCtrl"
-    })
-
-    $urlRouterProvider.otherwise('');
-
-})
+});
